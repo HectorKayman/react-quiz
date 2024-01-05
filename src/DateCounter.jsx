a1c4f351e6444ce0218a85815c29f68e825d3e70
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2024-01-06 01:33:51 +0530

    Use useReducer in place of useState for the 'count' variable

--- src/DateCounter.jsx
+++ src/DateCounter.jsx
@@ -1,33 +1,50 @@
-import { useState } from "react";
+import { useReducer, useState } from 'react';
+
+function reducer(state, action) {
+  console.log(state, action);
+  if (action.type === 'inc') return state + 1;
+  if (action.type === 'dec') return state - 1;
+  if (action.type === 'setCount') return action.payload;
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  // Using useReducer instead, to pass 'reducer' function and initial state
+  // 'dispatch' function is used to call the 'reducer' function or update it
+  const [count, dispatch] = useReducer(reducer, 0);
+
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
-  const date = new Date("june 21 2027");
+  const date = new Date('june 21 2027');
   date.setDate(date.getDate() + count);
 
   const dec = function () {
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
+
+    dispatch({ type: 'dec' });
   };
 
   const inc = function () {
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
+
+    dispatch({ type: 'inc' });
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    // setCount(Number(e.target.value));
+
+    dispatch({ type: 'setCount', payload: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
