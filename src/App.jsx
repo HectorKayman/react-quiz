524cf4ae21edc21a64780ad48369ba88f4515788
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2024-01-09 02:16:48 +0530

    Fetch data from the json server using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
 import Header from './Header';
 import MainComp from './MainComp';
 
 export default function App() {
+  useEffect(function () {
+    fetch('http://localhost:8140/questions')
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <div className="app">
       <Header />
