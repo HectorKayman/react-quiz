383127c0741ff8391dcb76a32e34af20e35060a9
Himanshu Kaushik <63892324+HectorKayman@users.noreply.github.com>
2024-01-09 03:06:25 +0530

    Resolve the data received from the fetch request using useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
 import { useEffect } from 'react';
 import Header from './Header';
 import MainComp from './MainComp';
+import { useReducer } from 'react';
+
+const initialState = {
+  questions: [],
+
+  //'loading', 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return {
+        ...state,
+        question: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    default:
+      throw new Error('Action unknown');
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   useEffect(function () {
     fetch('http://localhost:8140/questions')
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.error(err));
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch(() => dispatch({ type: 'dataFailed' }));
   }, []);
 
   return (
